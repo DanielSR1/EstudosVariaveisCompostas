b90955241bd277b70d3a99b8bf99fa8bfab929ad
DanielSR1 <danielsrcontato@gmail.com>
2022-03-20 01:26:42 -0300

    primeiros estudos tuplas

---
+++ 01-lanche.py
@@ -0,0 +1,6 @@
+lanche=('suco', 'queijo', 'pudim', 'pizza')
+for comida in lanche:   
+    print(f'eu vou comer {comida}')
+for cont in range (0, len (lanche)):
+    print(f'eu vou comer {lanche[cont]} na posição {cont}')
+print('BASTANTE COMIDA HEM')
