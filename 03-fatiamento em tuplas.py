9ce6f5099ce38a438737e029f21efcbf557d5d4c
DanielSR1 <danielsrcontato@gmail.com>
2022-03-20 22:43:02 -0300

    fatiamento com tuplas

---
+++ 03-fatiamento em tuplas.py
@@ -0,0 +1,5 @@
+lista='Daniel', 'Samuel','Rafael', 'Gabriel', 'Natanael'
+print(f'os três primeiros nomes da lista são {lista[0:3]}')
+print(f'os três primeiros nomes da lista são {lista[-3:]}')
+print(f'os nomes na ordem alfabéica são {sorted(lista)}')
+print(f'o nome "Daniel" está na {lista.index("Daniel")+1}º posição da LISTAGEM')
