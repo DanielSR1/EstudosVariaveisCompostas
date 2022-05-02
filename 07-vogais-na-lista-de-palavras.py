09bc0f483c8e44d3daf0129404420a2099cb7f94
Daniel Silva <93423106+DanielSR1@users.noreply.github.com>
2022-05-02 02:36:34 -0300

    Delete 07-vogais-na-lista-de-palavras.py

--- 07-vogais-na-lista-de-palavras.py
+++
@@ -1,7 +0,0 @@
-#NESSE EXERCICIO DESENVOLVI UM PROGRAMA CAPAZ DE DIZER QUAIS AS VOGAIS DE UMA PALAVRA EM UMA TUPLA.
-palavras=('aprender', 'dinheiro', 'curso', 'teclado')
-for p in palavras:
-    print(f'\nNa palavras {p} temos as vogais:', end=' ')
-    for letra in p:
-        if letra.lower() in "aeiou":
-            print(f'{letra}', end=' ')
