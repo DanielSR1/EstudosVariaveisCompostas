e0f3157d08ee27f5ee722c9c6607613c500c0cc3
Daniel Silva <93423106+DanielSR1@users.noreply.github.com>
2022-05-02 02:36:27 -0300

    Delete 06-lista-de-compras.py

--- 06-lista-de-compras.py
+++
@@ -1,9 +0,0 @@
-lista=('caderno', 15.50,
-'mochila', 12.55,
-'kit de escrever', 23.44,
-'livros', 23.44)
-for pos in range(0, len(lista)):
-    if pos % 2==0:
-        print(f'{lista[pos]:.<30}', end=' ')
-    else:
-        print(f'R$ {lista[pos]:>5.2f}')
